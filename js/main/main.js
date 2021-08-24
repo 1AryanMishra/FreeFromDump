daba4f13355ff5d4e8952f693d88e66d853e7092
Aryan Mishra <mishra321123@gmail.com>
2021-08-24 11:32:28 +0530

    aa some changes

---
+++ js/main/main.js
@@ -0,0 +1,31 @@
+var index = document;
+
+const themeColors = {
+    dark:{
+            DinputFont : "#000",
+            DheaderBg : "#151515",
+            DlogoFont : "#fff",
+            Dbase : "#000",
+            DbaseFont : "#fff",
+            DboxShadow : "#fff",
+            inputBoxShadow : "#909090",
+            buttonBoxShadow : "#fff",
+            inputBg: "#fff"
+        },
+
+    white : {
+            WinputFont : "#fff",
+            Wbase : "#d4d8dc",
+            WbaseFont : "blue",
+            WheaderBg : "rgba(255, 255, 255, 0.4)",
+            WlogoFont : "blue",
+            WboxShadow : "#000",
+            inputBoxShadow : "#909090",
+            buttonBoxShadow : "#fff",
+            inputBg: "#fff"
+        }
+    }
+    
+
+var darkSwitch = index.getElementById("switch");
+console.log(themeColors.dark.Dbase);

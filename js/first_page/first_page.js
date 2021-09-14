027156561345800447f4bbb934ee5688ddc0dac0
Aryan Mishra <mishra321123@gmail.com>
2021-09-14 18:46:21 +0530

    separated header footer css file

--- js/first_page/first_page.js
+++ js/first_page/first_page.js
@@ -29,12 +29,13 @@ function description(desc, prereq){
 }
 
 
-
+// To be added in resource_container before description
+//<iframe class = "demo_player" src="https://www.youtube.com/embed/${course_detail.videoId}?start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 function resource_container(course_detail){
     return (
         `<li class = "resource_details">
-            <iframe class = "demo_player" width="560" height="315" src="https://www.youtube.com/embed/${course_detail.videoId}?start=2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <img class = "demo_player" src = "../images/resources/beginner/css/courses/f.jpg" alt = "course demo player">
             ${description(course_detail.description, course_detail.prerequisites)}
         </li>`
     )

aef68f33987bb246f9077672abdf46fb795618c9
Aryan Mishra <mishra321123@gmail.com>
2021-10-07 11:49:05 +0530

    display username

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -15,11 +15,26 @@ const prov_btn = document.querySelectorAll('.login_provider');
 prov_btn.forEach((f) => {
     f.addEventListener('click', () => {
         signInWithPopup(auth, Google).then((result) => {
-            console.log(result.user.displayName);
-            console.log(result.user.email);
+            document.querySelector('.sign_in').style.display = 'none';
+            document.querySelector('.sign_in_box').style.display = 'none';
+            document.querySelector('.greet_user').style.display = 'block';
+            document.querySelector('.greet_user').textContent = `Hello, ${getFirstName(result.user.displayName)}`;
         }).catch((err) => {
             console.log(err);
             document.querySelector('.retry_msg').classList.toggle('display_err');
         })
     })
 })
+
+function getFirstName(name){
+    var fname = '';
+    for (let i = 0; i < name.length; i++) {
+        if(name[i] == " "){
+            break;
+        }
+        else{
+            fname += name[i];
+        }
+    }
+    return fname;
+}

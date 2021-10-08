c623947701b8b37e601e377475bacc5fdb958368
Aryan Mishra <mishra321123@gmail.com>
2021-10-08 13:50:02 +0530

    issue fixed

--- js/user/user.js
+++ js/user/user.js
@@ -8,8 +8,6 @@ onAuthStateChanged(auth, (user) => {
     if(user){
         const oldUser = getDocs(query(collection(db, 'users'), where("uid", "==", `${user.uid}`)));
         oldUser.then((response) => {
-            console.log(response);
-            console.log(response._snapshot);
             if(response._snapshot.docChanges.length > 1){
                 response.forEach((d) => {
                     console.log("User Exists.");

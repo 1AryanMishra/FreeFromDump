e5f227fdc0f8734c1869e4741abe9bc7bd8d7765
Aryan Mishra <mishra321123@gmail.com>
2021-10-08 13:02:23 +0530

    test user

--- js/user/user.js
+++ js/user/user.js
@@ -1,6 +1,6 @@
 import db from '../../database/firestore.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
-import { doc, collection, where, getDoc, setDoc, query } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+import { doc, collection, where, getDoc, getDocs, setDoc, query } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 
 const auth = getAuth();
 
@@ -17,18 +17,18 @@ onAuthStateChanged(auth, (user) => {
         console.log("Setting User Data...");
         const setting = setDoc(doc(db, "users", `${user.uid}`), data);
         setting.then(() => {
-            const UserData = getDoc(query(collection(db, 'users'), where("uid", "=", `${user.uid}`)));
+            const UserData = getDocs(query(collection(db, "users"), where("uid", "==", `${user.uid}`)));
             UserData.then((response) => {
                 console.log("Inside UserData.then ");
-                if(response.data()){
-                    console.log("User.data() is NOT null");
-                    console.log("logging UserData response", response.data());
-                }
-                else{
-                    console.log("User.data() IS null");
-                }
-            }).catch((err) => {
-                console.log("User Does not Exists.", err);
+                response.forEach((f) => {
+                    if(response.data()){
+                        console.log("User.data() is NOT null");
+                        console.log("logging UserData response", f.data());
+                    }
+                    else{
+                        console.log("User.data() IS null");
+                    }
+                })
             })
         }).catch((err) => {
             console.log("Error while setting User Data : ",err);

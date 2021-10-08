0f873c126dedb9ef0c9c2c8afa5306fe6a980843
Aryan Mishra <mishra321123@gmail.com>
2021-10-08 13:26:44 +0530

    a test

--- js/user/user.js
+++ js/user/user.js
@@ -8,26 +8,30 @@ onAuthStateChanged(auth, (user) => {
     if(user){
         const oldUser = getDocs(query(collection(db, 'users'), where("uid", "==", `${user.uid}`)));
         oldUser.then((response) => {
-            response.forEach((d) => {
-                console.log("User Exists.");
-                const userData = {
-                    name : `${d.data().name}`,
-                    email : `${d.data().email}`,
-                    uid : `${d.data().uid}`
-                }
-                console.log(userData);
-            })
+            if(response){
+                response.forEach((d) => {
+                    console.log("User Exists.");
+                    const userData = {
+                        name : `${d.data().name}`,
+                        email : `${d.data().email}`,
+                        uid : `${d.data().uid}`
+                    }
+                    console.log(userData);
+                })
+            }
+            else{
+                console.log("User does NOT exists.");
+                console.log("Setting User Data...");
+                const setting = setDoc(doc(db, 'users', `${user.uid}`), {
+                    name : `${user.displayName}`,
+                    uid : `${user.uid}`,
+                    email : `${user.email}`,
+                    course : `${JSON.parse(sessionStorage.getItem("course"))}`,
+                    level : `${JSON.parse(sessionStorage.getItem("level"))}`
+                })
+            }
         }).catch((err) => {
-            console.log("User does NOT exists.");
             console.log(err);
-            console.log("Setting User Data...");
-            const setting = setDoc(doc(db, 'users', `${user.uid}`), {
-                name : `${user.displayName}`,
-                uid : `${user.uid}`,
-                email : `${user.email}`,
-                course : `${JSON.parse(sessionStorage.getItem("course"))}`,
-                level : `${JSON.parse(sessionStorage.getItem("level"))}`
-            })
         })
     }
     else{

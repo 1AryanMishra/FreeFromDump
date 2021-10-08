673c1e8de56088b883b00af25cdb0c07597b2670
Aryan Mishra <mishra321123@gmail.com>
2021-10-08 13:32:59 +0530

    a test

--- js/user/user.js
+++ js/user/user.js
@@ -8,6 +8,7 @@ onAuthStateChanged(auth, (user) => {
     if(user){
         const oldUser = getDocs(query(collection(db, 'users'), where("uid", "==", `${user.uid}`)));
         oldUser.then((response) => {
+            console.log(response);
             if(response){
                 response.forEach((d) => {
                     console.log("User Exists.");

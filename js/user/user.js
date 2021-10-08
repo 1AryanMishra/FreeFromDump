7af000bf51ff178a3601dd98e812c86546ae1514
Aryan Mishra <mishra321123@gmail.com>
2021-10-08 21:22:46 +0530

    user greet functionality

--- js/user/user.js
+++ js/user/user.js
@@ -4,25 +4,54 @@ import { doc, collection, where, getDoc, getDocs, setDoc, query } from "https://
 
 const auth = getAuth();
 
+
+
+function getFirstName(name){
+    var fname = '';
+    for (let i = 0; i < name.length; i++) {
+        if(name[i] == " "){
+            break;
+        }
+        else{
+            fname += name[i];
+        }
+    }
+    return fname;
+}
+
+
+
 onAuthStateChanged(auth, (user) => {
     if(user){
+
+        //======= User Greeting =======
+        const greet_user = document.querySelector('.greet_user');
+        greet_user.style.display = 'flex';
+        greet_user.querySelector('.user_dp').src = `${user.photoURL}`;
+        greet_user.querySelector('.username').textContent = `Hello, ${getFirstName(user.displayName)}`;
+
+        //======= SignOut Functionality =======
+        document.querySelector('.sign_out').addEventListener('click', () => {
+            signOut(auth).then(() => {
+                document.querySelector('.sign_out').style.display = 'none';
+                document.querySelector('.sign_in_box').style.display = 'flex'
+                document.querySelector('.greet_user').style.display = 'none';
+                document.querySelector('.retry_msg').style.display = 'none';
+
+                signIn();
+            })
+            .catch((err) => {
+                document.querySelector('.sign_out_err').classList.toggle('sign_out_err_visible');
+            })
+        })
+
+        //======= User Data Fetching/Setting Processing =======
         const oldUser = getDocs(query(collection(db, 'users'), where("uid", "==", `${user.uid}`)));
         oldUser.then((response) => {
-            console.log(response);
             if(response._snapshot.docChanges.length >= 1){
-                response.forEach((d) => {
-                    console.log("User Exists.");
-                    const userData = {
-                        name : `${d.data().name}`,
-                        email : `${d.data().email}`,
-                        uid : `${d.data().uid}`
-                    }
-                    console.log(userData);
-                })
+                
             }
             else{
-                console.log("User does NOT exists.");
-                console.log("Setting User Data...");
                 const setting = setDoc(doc(db, 'users', `${user.uid}`), {
                     name : `${user.displayName}`,
                     uid : `${user.uid}`,

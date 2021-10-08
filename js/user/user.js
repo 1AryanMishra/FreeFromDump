72ee8a3b6239eaab7ed7ea4d2878600a209249b3
Aryan Mishra <mishra321123@gmail.com>
2021-10-08 12:22:07 +0530

    idk

--- js/user/user.js
+++ js/user/user.js
@@ -1,14 +1,14 @@
 import db from '../../database/firestore.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js';
-import { doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+import { doc, collection, where, getDoc, setDoc, query } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 
 const auth = getAuth();
 
 onAuthStateChanged(auth, (user) => {
     if(user){
-        const UserData = getDoc(doc(db, 'users', `${user.uid}`));
+        const UserData = getDoc(query(collection(db, 'users'), where("uid", "=", `${user.uid}`)));
         UserData.then((response) => {
-            if(response != null){
+            if(response.data()){
                 console.log("logging UserData response", response.data());
             }
             else{
@@ -21,7 +21,8 @@ onAuthStateChanged(auth, (user) => {
                 console.log(data);
             }
         }).catch((err) => {
-            location.reload();
+            console.log("User Does not Exists.");
+            //location.reload();
         })
     }
     else{
